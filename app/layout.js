3ce242b8adb4344bfbbc9ed1e851dd09afeed308
mbozkayaGitHub <mbozkaya27@gmail.com>
2024-05-15 20:42:12 +0300

    update

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,21 @@
-import {Inter} from "@next/font/google"
-import "@/styles/global.css"
-import "@/styles/reset.css"
+// import {Inter} from "@next/font/google";
 
+import "@/styles/global.css";
+import "@/styles/reset.css";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
-const interFontfamily = Inter({subsets: ["latin"]});
+// const interFontfamily = Inter({subsets: ["latin"]});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="{interFontFamily.className}">
-      <body className="container" >{children}</body>
+    <html lang="tr" className="{interFontFamily.className}">
+      <body className="container">
+        <Header />
+        <main>{children}</main>
+
+        <Footer />
+      </body>
     </html>
   );
 }

2d9ad93073f44e94434d6eb842b23f35e81fe199
mbozkayaGitHub <mbozkaya27@gmail.com>
2024-06-03 23:19:10 +0300

    update

---
+++ components/movies-section/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import styles from "./styles.module.css";
+
+function MoviesSection({ title, movies, vote }) {
+  return (
+    <div className={styles.moviesSection}>
+      <h3 className={styles.title}>{title}</h3>
+      <h3 className={styles.vote}>{vote}</h3>
+      <div className={styles.movies}>
+        {movies.map((movie) => (
+          <div className={styles.movie} key={movie.id}>
+            <Link href={`/movie/${movie.id}`}>
+              <Image
+                fill
+                unoptimized
+                alt={movie.title}
+                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+// export { MoviesSectionLoading } from "./loading";
+export { MoviesSection };
